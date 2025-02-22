909ccb566e043b4c1b162fd9a17174c1f0259a72
Inser-coder <sayonarabazzar@gmail.com>
2025-02-22 16:57:35 +0530

    updated with commits

--- webscrape_strard.py
+++ webscrape_strard.py
@@ -9,30 +9,30 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-class StubHubSpider(scrapy.Spider):
-    name = "stubhub_sports"
+class StubHubSpider(scrapy.Spider):#this is declaration of spider class
+    name = "stubhub_sports" #name of spider
 
     start_urls = [
         "https://www.stubhub.com/explore?lat=MjUuNDQ3ODkwMw%3D%3D&lon=LTgwLjQ3OTIxOTY%3D&to=253402300799999&tlcId=2"
     ]
 
-    sport_data = []
+    sport_data = []   #for storing the data
 
-    def __init__(self):
+    def __init__(self):  #selenuim chrome options
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)# automatically download the chrome driver
 
     def parse(self, response, **kwargs):
         self.driver.get(response.url)
         time.sleep(5)
-
+        #xpath from the node
         events = self.driver.find_elements(By.XPATH, "//a[contains(@class, 'hIVWQw')]")
-
+        # looping the same xpath for all events
         for event in events:
             try:
                 name = event.find_element(By.XPATH, ".//p[contains(@class, 'gSA-DZE')]").text
@@ -41,6 +41,7 @@ class StubHubSpider(scrapy.Spider):
 
                 place = event.find_element(By.XPATH, ".//p[contains(@class,'bsWxzg')][2]").text
                 image = event.find_element(By.XPATH, ".//img[contains(@class,'fpvUjg')]").get_attribute("src")
+                #storing the data in dict
                 event_data = {
                     "name": name,
                     "date": date,
@@ -49,15 +50,15 @@ class StubHubSpider(scrapy.Spider):
 
                 }
 
-                self.sport_data.append(event_data)
+                self.sport_data.append(event_data)# appending the dict to empty list
 
-            except Exception as e:
+            except Exception as e:# try and cath block for any exception handling
                 self.logger.error(f"Error extracting event details: {e}")
                 continue
 
     def closed(self, reason):
-        file_path = os.path.join(os.getcwd(), "sports_data.json")
+        file_path = os.path.join(os.getcwd(), "sports_data.json")#json file opening
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump(self.sport_data, f, indent=4, ensure_ascii=False)
 
-        self.driver.quit()
+        self.driver.quit()# driver closing
